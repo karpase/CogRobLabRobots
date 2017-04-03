86446123793eb091924d59b9eb82434951c45fd3
Erez Karpas <karpase@gmail.com>
2017-04-03 14:58:31 +0300

    Update setup_dependencies.sh

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -2,3 +2,6 @@
 
 sudo apt-get install ros-indigo-moveit-robots ros-indigo-moveit
 
+sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
+sudo apt-get update 
+sudo apt-get install libopencv-nonfree-dev
