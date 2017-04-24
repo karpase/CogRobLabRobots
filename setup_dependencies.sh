22a0e18d36082e3b9d9ab22e1e2e23216b112b3c
Erez Karpas <karpase@gmail.com>
2017-04-24 09:20:22 +0300

    Update setup_dependencies.sh

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install ros-indigo-moveit-robots ros-indigo-moveit ros-indigo-gazebo-ros-control
+sudo apt-get install ros-indigo-moveit-robots ros-indigo-moveit ros-indigo-gazebo-ros-control ros-indigo-twist-mux
 
 sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
 sudo apt-get update 
